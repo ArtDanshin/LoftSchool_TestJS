10789d2f59659f4c62d19c575a6a6bb08cd0a722
SilverDragoon <silvir007@gmail.com>
2016-03-03 00:48:19 +0300

    It's Run!

--- app/js/main.js
+++ app/js/main.js
@@ -1,11 +1,19 @@
 ;(function () {
 
-	var elems = document.querySelectorAll('.arcad-list__item');
+	var elems = document.querySelectorAll('.arcad-list-item__title');
 
 	for (var i = 0; i < elems.length; i++) {
-		if (elems[i].className('active').length == []) {
-			elems[i].className += " active"
-		}
+		(function(i) {
+			elems[i].addEventListener('click', function(){
+				if (elems[i].classList.contains('active')) {
+					elems[i].classList.remove('active');
+					elems[i].nextElementSibling.classList.add('hidden');
+				} else {
+					elems[i].classList.add('active');
+					elems[i].nextElementSibling.classList.remove('hidden');
+				}
+			})
+		})(i)
 	}
 
 })();
