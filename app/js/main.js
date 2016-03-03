6dbf22eba9982e64b1db11b61cb8b3da0f186176
SilverDragoon <silvir007@gmail.com>
2016-03-03 23:12:14 +0300

    Refactoring JS

--- app/js/main.js
+++ app/js/main.js
@@ -5,12 +5,15 @@
 	for (var i = 0; i < elems.length; i++) {
 		(function(i) {
 			elems[i].addEventListener('click', function(){
-				if (elems[i].classList.contains('active')) {
-					elems[i].classList.remove('active');
-					elems[i].nextElementSibling.classList.add('hidden');
+				var classTitle = elems[i].classList,
+					classListItems = elems[i].nextElementSibling.classList;
+
+				if (classTitle.contains('active')) {
+					classTitle.remove('active');
+					classListItems.add('hidden');
 				} else {
-					elems[i].classList.add('active');
-					elems[i].nextElementSibling.classList.remove('hidden');
+					classTitle.add('active');
+					classListItems.remove('hidden');
 				}
 			})
 		})(i)
